608ea78f698c49da0931642ac6e8dcb62377ab9b
aijinrjinr <72067388+aijinrjinr@users.noreply.github.com>
2022-07-19 15:21:38 -0700

    Add files via upload
    delete unused paths

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-JOINT_path = './003/JOINT_list.npy'
-SC_path = './003/SC_list.npy'
-FC_path = './003/FC_list.npy'
-label_file = './003/control_subject_label.csv'
-train_idx_fold = './003/train_idx.npy'
-test_idx_fold = './003/test_idx.npy'
+JOINT_path = './data/JOINT_list.npy'
+label_file = './data/control_subject_label.csv'
+Machine_path = './data/machine_list.npy'
+Concat_Feature_path = './data/F_list_deg&cen.npy'
 
+train_idx_fold = './data/train_idx.npy'
+test_idx_fold = './data/test_idx.npy'
